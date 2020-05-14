836104fc7650fd0afc6326b858a593d8e308045f
Tsyupryk Roman <tsyupryk.roman@gmail.com>
2020-05-14 16:36:26 +0200

    Rename project name.

--- src/main/java/com/trl/discoveryserver/DiscoveryServerApplication.java
+++ src/main/java/com/trl/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.trl.eurekaserver;
+package com.trl.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class DiscoveryServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(DiscoveryServerApplication.class, args);
     }
 
 }
--- src/test/java/com/trl/discoveryserver/DiscoveryServerApplicationTests.java
+++ src/test/java/com/trl/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.trl.eurekaserver;
+package com.trl.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class DiscoveryServerApplicationTests {
 
     @Test
     void contextLoads() {
